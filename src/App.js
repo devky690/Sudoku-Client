a63972a1b7010227327e313299d27e0550cf7653
Kyle Abrahams <kabrah5@students.towson.edu>
2021-07-15 13:46:06 -0400

    conditional rendering for modal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React, {useState} from "react";
 import Board from "./components/board.js";
+import Modal from './components/modal.js';
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
   return (
     <div className="App">
-      <Board> </Board>
+      <Board showModal = {showModal} setShowModal={setShowModal}> </Board>
+      {showModal && <Modal> </Modal>}
     </div>
   );
 }
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import '../styles/modal.css';
 
 const Modal = () => {
     return (
-        <div>
-            
+        <div className="modal-container">
+           
         </div>
     )
 }
